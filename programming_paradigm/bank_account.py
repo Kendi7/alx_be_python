02515ac9f70512bc383348137f9778f26995145d
Kennedy Otieno <105186884+Kendi7@users.noreply.github.com>
2025-08-03 22:13:02 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
-    def __init__(self):
-        self.account_balance = 0
+    def __init__(self, starting_balance=0):
+        self.account_balance = starting_balance
 
     def deposit(self, amount):
         self.account_balance += amount
@@ -14,4 +14,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        # Match the exact string expected
+        print(f"Current Balance: ${self.account_balance:.2f}")
